41f1b9664b6655f5a3ad32120268c111b41be081
amcdowell <armcdowell@hotmail.com>
2013-04-17 10:26:40 +1200

    Updated to remove/hide Map tabs and menu items from SOLA Tonga

--- extension/user_roles.sql
+++ extension/user_roles.sql
@@ -11,3 +11,6 @@ INSERT INTO system.approle_appgroup (approle_code, appgroup_id)
  WHERE NOT EXISTS (SELECT approle_code FROM system.approle_appgroup rg
                  WHERE  rg.approle_code = r.code
 				 AND    rg.appgroup_id = 'super-group-id'));
+				 
+-- Remove the View Map role - the Map is not used in Tonga (yet)
+DELETE FROM system.approle_appgroup WHERE approle_code = 'ViewMap';
